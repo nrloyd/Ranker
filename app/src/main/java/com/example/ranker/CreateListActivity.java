c64dea040284d50755f6e1c72a4df54c27c5a492
Nick Loyd <nickloyd1997@gmail.com>
2020-05-04 03:10:02 -0700

    Make Create List screen functional

--- app/src/main/java/com/example/ranker/CreateListActivity.java
+++ app/src/main/java/com/example/ranker/CreateListActivity.java
@@ -2,7 +2,11 @@ package com.example.ranker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateListActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class CreateListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_list);
+
+        Button createButton = findViewById(R.id.create_list_button);
+        createButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editListTitle = (EditText) findViewById(R.id.edit_list_name);
+                EditText editListContent = (EditText) findViewById(R.id.edit_list_content);
+                String listTitle = editListTitle.getText().toString();
+                String listContent = editListContent.getText().toString();
+                DataManager.getInstance().getGroups().add(new RankGroup(listTitle,listContent.split("\\r?\\n")));
+
+                Intent intent = new Intent(CreateListActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
